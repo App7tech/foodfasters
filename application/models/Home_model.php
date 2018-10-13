9bf74068b5b7d95d4bb8544ae817720db80c606f
DELL\UDAY KUMAR <uday.veduruvada@bluplateau.com>
2018-10-13 10:26:11 +0530

    seller admin login method added

--- application/models/Home_model.php
+++ application/models/Home_model.php
@@ -31,5 +31,37 @@ class Home_model extends CI_Model{
 		}
 		
 	}
+
+	public function seller_admin_login($post){
+		$password = $post['password'];
+		$email = $post['email'];
+		$e_where = array('restaurant_email'=>$email);
+		$this->db->where($e_where);
+		$e_query = $this->db->get('restaurant_add');		
+		if($e_query->num_rows()!=0){
+			//check email with password
+			$where = array('restaurant_email'=>$email,'password'=>md5($password));
+			$this->db->where($where);
+			$query = $this->db->get('restaurant_add');
+			if($query->num_rows()!=0){
+				//success login
+				$result = $query->result_array();
+				$this->session->set_userdata('email',$result);
+				$data['status'] = true;
+				$data['message'] = $result;
+				return $data;
+			}else{
+				//password error
+				$data['status'] = false;
+				$data['message'] = "Invalid Password!";
+				return $data;
+			}
+		}else{
+			//email error
+			$data['status'] = false;
+			$data['message'] = "Invalid Email!";
+			return $data;
+		}
+	}
 }
 ?>
