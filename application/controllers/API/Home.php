b40f82ff5f46dfc6b17b22709c3b13aabf32870e
UDAY KUMAR <udaykumarks84@gmail.com>
2018-10-23 20:15:12 +0530

    home sliders api

---
+++ application/controllers/API/Home.php
@@ -0,0 +1,29 @@
+<?php
+require(APPPATH.'/libraries/REST_Controller.php');
+class Home extends REST_Controller
+{    	
+	public function __construct()
+	{
+		parent::__construct();
+		header('Access-Control-Allow-Origin: *');
+		header("Access-Control-Request-Method: *");
+		header("Access-Control-Request-Headers: *");
+		header("Access-Control-Allow-Headers: Origin, Content-type, X-Auth-Token, Authorization");
+		$this->load->model('super_admin/Main_model');
+	}	
+
+	public function main_sliders_post()
+	{	
+		$data['status'] = true;
+		$data['message'] = $this->Main_model->slider_data();
+		$path = base_url()."images/sliders/";
+		$i = 0;
+		foreach ($data['message'] as $slider) {
+			$data['message'][$i]['image'] = $path.$slider['image'];
+			$i++;
+		}
+		$this->response($data, 200);
+	}
+
+}
+	
