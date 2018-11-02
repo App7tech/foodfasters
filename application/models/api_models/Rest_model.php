20879461aae29381177097f0862f1f45cc1f1b2d
UDAY KUMAR <udaykumarks84@gmail.com>
2018-11-02 19:49:36 +0530

    get restaurants api and design changes

--- application/models/api_models/Rest_model.php
+++ application/models/api_models/Rest_model.php
@@ -6,18 +6,27 @@ class Rest_model extends CI_Model{
 		$q=$this->db->query("select * , (6371 * acos( cos( radians($v1)) * cos( radians( Latitude ) ) * cos( radians( longitude ) - radians($v2))  + sin( radians($v1) ) * sin( radians( Latitude ) ) ) ) AS distance FROM  restaurant_add HAVING distance < 25 ORDER BY distance LIMIT 0 , 20");
 		$r=$q->result_array();
 		$i = 0;
-		foreach ($r as $row) {
-			$data[$i]['id'] = $row['id'];
-			$data[$i]['name'] = $row['name'];
-			$data[$i]['mobile'] = $row['mobile'];
-			$data[$i]['restaurant_name'] = $row['restaurant_name'];
-			$data[$i]['restaurant_email'] = $row['restaurant_email'];
-			$data[$i]['token'] = $row['token'];
-			$data[$i]['restaurant_address'] = $row['restaurant_address'];
-			$data[$i]['restaurant_city'] = $row['restaurant_city'];
-			$data[$i]['latitude'] = $row['Latitude'];
-			$data[$i]['longitude'] = $row['longitude'];
-			$data[$i]['open_days'] = unserialize($row['open_days']);
+		if(!empty($r)){
+			foreach ($r as $row) {
+				$data[$i]['id'] = $row['id'];
+				$data[$i]['name'] = $row['name'];
+				$data[$i]['mobile'] = $row['mobile'];
+				$data[$i]['restaurant_name'] = $row['restaurant_name'];
+				$data[$i]['restaurant_email'] = $row['restaurant_email'];
+				$data[$i]['image'] = base_url()."images/restaurants/".$row['image'];
+				$data[$i]['token'] = $row['token'];
+				$data[$i]['restaurant_address'] = $row['restaurant_address'];
+				$data[$i]['restaurant_city'] = $row['restaurant_city'];
+				$data[$i]['latitude'] = $row['Latitude'];
+				$data[$i]['longitude'] = $row['longitude'];
+				$data[$i]['distance'] = $row['distance'];
+				$data[$i]['open_days'] = unserialize($row['open_days']);
+				$data[$i]['open_time'] = $row['open_time'];
+				$data[$i]['close_time'] = $row['close_time'];
+				$i++;
+			}
+		}else{
+			$data = array();
 		}
 		return $data;
 	}
